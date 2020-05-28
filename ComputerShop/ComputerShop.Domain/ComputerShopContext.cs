c12a1d9cfe0cabf6ff0b913827ef68f0061fd87e
shupimumi <shupimumi@gmail.com>
2020-05-28 13:59:12 +0400

    ComputerShop

---
+++ ComputerShop/ComputerShop.Domain/ComputerShopContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ComputerShop.Domain
+{
+	public class ComputerShopContext : DbContext
+	{
+		public ComputerShopContext(DbContextOptions<ComputerShopContext> options) : base(options)
+		{
+			var customer = new Customer();
+		}
+		public DbSet<Customer> Customers { get; set; }
+	
+	}
+}
---
+++ ComputerShop/ComputerShop.Domain/Entities/Customer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ComputerShop.Domain
+{
+	public class Customer
+	{
+		public Guid Id { get; set; }
+		public string Surname { get; set; }
+		public string Name { get; set; }
+		public string Phone { get; set; }
+		public string Passport { get; set; }
+		public string SecondName { get; set; }
+		public string Email { get; set; }
+	}
+}
